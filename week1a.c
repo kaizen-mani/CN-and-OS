43af8ea549c60f7a46bce86db871231050e51827
kaizen-mani <m.ankithreddy2005@gmail.com>
2025-06-05 21:12:07 +0530

    Update week1a.c

--- week1a.c
+++ week1a.c
@@ -3,16 +3,11 @@
 void bitstuffing(int N, int arr[]) {
     int count = 0;
     int i, j = 0;
-
-    // Create a new array to store the stuffed bits
-    int stuffedArr[2 * N]; // worst-case size (maximum stuffing)
-
-    // Traverse the original array and apply bit-stuffing
+    int stuffedArr[2 * N];
     for (i = 0; i < N; i++) {
         stuffedArr[j++] = arr[i];
         if (arr[i] == 1) {
             count++;
-            // If we encounter five consecutive 1's, we insert a 0
             if (count == 5) {
                 stuffedArr[j++] = 0;
                 count = 0;
@@ -21,8 +16,6 @@ void bitstuffing(int N, int arr[]) {
             count = 0;
         }
     }
-
-    // Print the stuffed array
     for (i = 0; i < j; i++) {
         printf("%d", stuffedArr[i]);
     }
